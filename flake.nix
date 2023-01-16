076d50ffbebe4af456db1e25b6dd322e702cef80
Akira Komamura <akira.komamura@gmail.com>
2023-01-17 07:46:25 +0900

    Add a bunch of inputs

--- flake.nix
+++ flake.nix
@@ -1,36 +1,78 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-  inputs.stable.url = "github:NixOS/nixpkgs/nixos-22.11";
-  inputs.unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    stable.url = "github:NixOS/nixpkgs/nixos-22.11";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  # Update home-manager when nixpkgs is updated
-  inputs.home-manager = {
-    url = "github:nix-community/home-manager";
-    # This doesn't work
-    # inputs.nixpkgs.follows = "unstable";
-    inputs.utils.follows = "flake-utils";
+    # Update home-manager when nixpkgs is updated
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # This doesn't work
+      # inputs.nixpkgs.follows = "unstable";
+      inputs.utils.follows = "flake-utils";
+    };
+
+    # I never use Darwin, but some flakes depend on it.
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs-stable.follows = "stable";
+      inputs.nixpkgs.follows = "unstable";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-images = {
+      url = "github:nix-community/nixos-images";
+      inputs.nixos-2211.follows = "nixpkgs";
+      inputs.nixos-unstable.follows = "unstable";
+    };
+
+    nixos-remote = {
+      url = "github:numtide/nixos-remote";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixos-images.follows = "nixos-images";
+      inputs.disko.follows = "disko";
+      inputs.treefmt-nix.follows = "treefmt-nix";
+    };
+
+    cachix-deploy-flake = {
+      url = "github:cachix/cachix-deploy-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.disko.follows = "disko";
+      inputs.home-manager.follows = "home-manager";
+      inputs.darwin.follows = "nix-darwin";
+      inputs.nixos-remote.follows = "nixos-remote";
+    };
+
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+    };
   };
-
-  # I never use Darwin, but some flakes depend on it.
-  inputs.nix-darwin = {
-    url = "github:LnL7/nix-darwin";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.pre-commit-hooks = {
-    url = "github:cachix/pre-commit-hooks.nix";
-    inputs.nixpkgs-stable.follows = "stable";
-    inputs.nixpkgs.follows = "unstable";
-    inputs.flake-utils.follows = "flake-utils";
-  };
-
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
-  };
-
-  inputs.emacs-overlay.url = "github:nix-community/emacs-overlay";
 
   outputs = {
     self,
