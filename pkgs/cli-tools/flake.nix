8f90fb321e9efcb1e374dbacbaefbf46b507c9fa
Akira Komamura <akira.komamura@gmail.com>
2024-10-04 00:34:20 +0900

    chore: Require the full nixpkgs dependency

--- pkgs/cli-tools/flake.nix
+++ pkgs/cli-tools/flake.nix
@@ -2,7 +2,7 @@
   description = "Custom CLI tools that can be integrated into Emacs and NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable?dir=lib";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     epubinfo.url = "github:akirak/epubinfo";
     squasher.url = "github:akirak/squasher";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.0.1.tar.gz";
