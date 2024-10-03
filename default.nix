2745e15660f1d1136c7979e938f81f9e17a393f8
Akira Komamura <akira.komamura@gmail.com>
2024-10-03 23:28:00 +0900

    docs: Add a description to default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,12 @@
-(
-  import
-  (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
-  )
-  {src = ./.;}
-)
-.defaultNix
+# This default.nix is kept for allowing non-flake access to the flake outputs
+# from some places. When using only some parts of the flake, it's nice to avoid
+# inclusion of indirect inputs.
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  }
+) { src = ./.; }).defaultNix
