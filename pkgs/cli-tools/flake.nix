cea296cc159e5436c0f6d9d74eec33799205a8a5
Akira Komamura <akira.komamura@gmail.com>
2024-10-04 20:21:38 +0900

    chore(pkgs/cli-tools): Remove flake-compat

--- pkgs/cli-tools/flake.nix
+++ pkgs/cli-tools/flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     epubinfo.url = "github:akirak/epubinfo";
     squasher.url = "github:akirak/squasher";
-    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.0.1.tar.gz";
   };
 
   outputs =
