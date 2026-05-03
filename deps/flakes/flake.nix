dc1c7082a8126f774237696d6601a347dfbd7e57
Akira Komamura <akira.komamura@gmail.com>
2026-05-03 17:14:51 +0900

    Add the inputs for rustfs

--- deps/flakes/flake.nix
+++ deps/flakes/flake.nix
@@ -1,8 +1,15 @@
 {
   inputs = {
+    # Should be the same as the one used in the main flake.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     epubinfo.url = "github:akirak/epubinfo";
     squasher.url = "github:akirak/squasher";
     pipelight.url = "github:pipelight/pipelight";
+    rustfs = {
+      url = "github:rustfs/rustfs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = _: { };
