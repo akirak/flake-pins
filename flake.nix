6933ca1c30f5249c598ed4a449031dbd2a7c4f5a
Akira Komamura <akira.komamura@gmail.com>
2026-07-25 10:43:01 +0900

    chore: Revert the input overrides

--- flake.nix
+++ flake.nix
@@ -28,13 +28,8 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
-    tsgo-effect = {
-      url = "github:Effect-TS/tsgo";
-      inputs = {
-        nixpkgsUnstable.follows = "nixpkgs";
-        typescript-go-src.url = "git+https://github.com/microsoft/typescript-go?submodules=1";
-      };
-    };
+    tsgo-effect.url = "github:Effect-TS/tsgo";
+    tsgo-effect.inputs.nixpkgsUnstable.follows = "nixpkgs";
 
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
