895a82a9d65bb520415b945778340348b196fde5
Akira Komamura <akira.komamura@gmail.com>
2026-09-16 20:28:38 +0900

    fix: Support internal references

--- registry.nix
+++ registry.nix
@@ -1,30 +1,26 @@
 {
   writeTextFile,
   lib,
-}: let
+}:
+let
   inherit (builtins) toJSON hasAttr;
   inherit (lib) mapAttrsToList importJSON;
 
   inherit (importJSON ./flake.lock) nodes;
 
-  flakes =
-    mapAttrsToList (
-      id: name: {
-        from = {
-          inherit id;
-          type = "indirect";
-        };
-
-        to =
-          nodes.${name}.locked;
-      }
-    )
-    nodes.root.inputs;
-in
-  writeTextFile {
-    name = "registry.json";
-    text = toJSON {
-      version = 2;
-      inherit flakes;
+  flakes = mapAttrsToList (id: names: {
+    from = {
+      inherit id;
+      type = "indirect";
     };
-  }
+
+    to = nodes.${if builtins.typeOf names == "string" then names else builtins.elemAt names 0}.locked;
+  }) nodes.root.inputs;
+in
+writeTextFile {
+  name = "registry.json";
+  text = toJSON {
+    version = 2;
+    inherit flakes;
+  };
+}
